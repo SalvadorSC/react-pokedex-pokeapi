71d51cd35eadc10de30120ffe5655ef06bfb0cb9
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-07-05 22:01:55 +0200

    Get pokemon by region, still not by order.

--- src/App.js
+++ src/App.js
@@ -10,17 +10,52 @@ import { PaginaPrincipal } from "./paginas/PaginaPrincipal";
 import { DatosAmigosContext } from "./contexts/DatosAmigosContext";
 function App() {
   const [allPokemon, setAllPokemon] = useState([]);
-  const urlAPI = "https://pokeapi.co/api/v2/pokemon?limit=20";
+  const [urlAPI, setUrlAPI] = useState("");
+  const [regionSelected, setRegionSelected] = useState("");
 
-  const llamadaListaPokemon = async (urlAPI) => {
-    const response = await fetch(urlAPI);
-    const pokemonsters = await response.json();
-    setAllPokemon(pokemonsters.results);
+  const getUrlAPI = (region) => {
+    switch (region) {
+      case "1":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?limit=151");
+        break;
+      case "2":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=151&limit=100");
+        break;
+      case "3":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=251&limit=135");
+        break;
+      case "4":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=386&limit=107");
+        break;
+      case "5":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=493&limit=156");
+        break;
+      case "6":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=649&limit=72");
+        break;
+      case "7":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=721&limit=88");
+        break;
+      case "8":
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?offset=809&limit=89");
+        break;
+      default:
+        setUrlAPI("https://pokeapi.co/api/v2/pokemon?limit=10");
+    }
   };
 
   useEffect(() => {
-    llamadaListaPokemon(urlAPI);
-  }, []);
+    const llamadaListaPokemon = async () => {
+      console.log(typeof regionSelected);
+      getUrlAPI(regionSelected);
+      console.log(urlAPI);
+
+      const response = await fetch(urlAPI);
+      const pokemonsters = await response.json();
+      setAllPokemon(pokemonsters.results);
+    };
+    llamadaListaPokemon();
+  }, [regionSelected, urlAPI]);
 
   return (
     <>
@@ -32,7 +67,10 @@ function App() {
               allPokemon,
             }}
           >
-            <Cabecera />
+            <Cabecera
+              regionSelected={regionSelected}
+              setRegionSelected={setRegionSelected}
+            />
             <Switch>
               <Route path="/" exact>
                 <Redirect to="/principal" />
